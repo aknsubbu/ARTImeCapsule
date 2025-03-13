6e5a8de6517267a7967b325cbb293c267c308d25
aknsubbu <aknsubbu@gmail.com>
2025-03-13 13:01:59 +0530

    update AR testing scene and add expo-dev-client dependency

--- app/(tabs)/artesting.tsx
+++ app/(tabs)/artesting.tsx
@@ -1,25 +1,56 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { ViroARScene, ViroARSceneNavigator } from "@viro-community/react-viro";
-import ARNote from "../../components/ARNote";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import {
+  ViroARScene,
+  ViroText,
+  ViroTrackingStateConstants,
+  ViroARSceneNavigator,
+  ViroTrackingReason,
+} from "@viro-community/react-viro";
 
-const ARTestScene: React.FC = () => {
+const HelloWorldSceneAR = () => {
+  const [text, setText] = useState("Initializing AR...");
+
+  function onInitialized(state: any, reason: ViroTrackingReason) {
+    console.log("guncelleme", state, reason);
+    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
+      setText("Hello World!");
+    } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
+      // Handle loss of tracking
+    }
+  }
+
   return (
-    <ViroARScene>
-      <ARNote />
+    <ViroARScene onTrackingUpdated={onInitialized}>
+      <ViroText
+        text={text}
+        scale={[0.5, 0.5, 0.5]}
+        position={[0, 0, -1]}
+        style={styles.helloWorldTextStyle}
+      />
     </ViroARScene>
   );
 };
 
-const ARTestingScreen: React.FC = () => {
+export default function ARPage() {
   return (
     <ViroARSceneNavigator
+      autofocus={true}
       initialScene={{
-        scene: () => <ARTestScene />,
+        scene: HelloWorldSceneAR,
       }}
-      style={{ flex: 1 }}
+      style={styles.f1}
     />
   );
-};
+}
 
-export default ARTestingScreen;
+var styles = StyleSheet.create({
+  f1: { flex: 1 },
+  helloWorldTextStyle: {
+    fontFamily: "Arial",
+    fontSize: 30,
+    color: "#ffffff",
+    textAlignVertical: "center",
+    textAlign: "center",
+  },
+});
